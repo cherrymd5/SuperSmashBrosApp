729a778536e755c92be894d694b8050cf753a963
molly namiotka <mollynamiotka@mollys-Air.fios-router.home>
2022-07-12 21:55:44 -0400

    Added put requst and returned all matches

--- App.py
+++ App.py
@@ -1,22 +1,49 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
-from db_connection import get_matches
+from db_connection import get_matches, write_match
+import requests
+
 
 app = Flask(__name__)
 
 # Get Matchups 
 @app.route("/", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World! Whooo</p>"
+    return "Hello, World! Whooo"
 
 @app.route("/get-all-matches", methods=['GET'])
 def get_all_matches():
-    print(get_matches()[0][0])
-    data = {
-        "player": get_matches()[0][3],
-        "opponent": get_matches()[0][4],
-        "result": get_matches()[0][5]
-    }
-    return jsonify(data)
+    # print(get_matches()[0][0])
+    response = get_matches() # [1, 5, 23, 53, [0, 2, 5]] --> index starts at 0
+    print(response[0][1])
+
+    result = []
+    for row in response: 
+        data = {
+            "player": row[3],
+            "opponent": row[4],
+            "result": row[5]
+        }
+        result.append(data)
+
+    return jsonify(result)
+
+    # json = javascript object notation 
 
 # Post new Matchup 
+@app.route("/put-match", methods=['PUT'])
+def put_match():
+    player = request.args.get('player')
+    opponent = request.args.get('opponent')
+    result = request.args.get('result')
+
+    print("{} {} {}".format(player, opponent, result))
+
+    write_match(player, opponent, result)
+
+    # print(request.args.get('player'))
+    return 'match posted'
+
+
+
+# DRY - Do not repeat
--- db_connection.py
+++ db_connection.py
@@ -28,3 +28,29 @@ def get_matches():
 
 
 
+def write_match(player, opponent, result):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                    password="Huntee123",
+                                    host="super-smash-bros.cjzzgdbitg4q.us-east-1.rds.amazonaws.com",
+                                    port="5432",
+                                    database="super_smash_bros_db")
+        cursor = connection.cursor()
+        query = "INSERT INTO smash_match_history(match_time, update_time, player_name, opponent_name, result) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, %s, %s, %s);"
+
+        cursor.execute(query, (player, opponent, result))
+        cursor.close()
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
+
